bf642d143e93861373e42c2a17373e2e14a35ba2
amey patil <ameypatil2820@gmail.com>
2026-05-06 10:51:09 +0530

    code updated

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,7 @@ app.use(cors());
 app.use(express.json());
 
 // Request Logger
+
 app.use((req, res, next) => {
     console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
     next();
